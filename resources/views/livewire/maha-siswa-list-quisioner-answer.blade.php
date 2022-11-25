c7995ddf52bc8d649946b16d6e71599dfe10fb92
Yudi Candra <yudhichandra02@gmail.com>
2022-11-25 22:44:47 +0700

    update table

--- resources/views/livewire/maha-siswa-list-quisioner-answer.blade.php
+++ resources/views/livewire/maha-siswa-list-quisioner-answer.blade.php
@@ -25,7 +25,7 @@
                             <ul style="list-style-type:disc;">
                                 @foreach ($answers as $item)
                                 @if ($question->getJawaban($item->option_question_id))
-                                <li>{{$question->getJawaban($item->option_question_id)}}</li>
+                                <li>{{$item->id}} - {{$question->getJawaban($item->option_question_id)}}</li>
                                 @endif
                                 @endforeach
 
