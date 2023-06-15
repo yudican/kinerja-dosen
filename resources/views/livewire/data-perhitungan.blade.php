1fba9320d077e3e1e1e5542490abf965ccdc1e19
user <user@users-MacBook-Pro.local>
2023-06-15 07:22:15 +0700

    update

--- resources/views/livewire/data-perhitungan.blade.php
+++ resources/views/livewire/data-perhitungan.blade.php
@@ -11,20 +11,7 @@
           </h4>
         </div>
       </div>
-      <div class="card">
-        <div class="card-body">
-          <div class="form-group">
-            <label for="my-input">Pilih Semester</label>
-            <select wire:model="data_semester_id" class="form-control" wire:change="selectSemester($event.target.value)">
-              <option value="">Pilih Semester</option>
-              @foreach ($semesters as $semester)
-              <option value="{{$semester->id}}">{{$semester->kode_semester}} -
-                {{$semester->nama_semester}}</option>
-              @endforeach
-            </select>
-          </div>
-        </div>
-      </div>
+
     </div>
     <div class="col-md-12">
       <livewire:table.data-jadwal-table params="{{$route_name}}" sort="hari_jadwal|asc" />
